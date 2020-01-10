458401d5c7bb8c2d9cff7d91cc73cd8284d8b02c
a18neigutcha <a18neigutcha@inspedralbes.cat>
2020-01-10 01:01:33 +0100

    Inicio del primer proyecto de node.js

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+console.log(add(1,2));
+console.log(substract(1,2));
+console.log(multiply(1,2));
+console.log(divede(1,2));
+console.log(divede(1,0));
+
+//** Nos quedamos en el min 24:02 URL: https://www.youtube.com/watch?v=BhvLIzVL8_o */
